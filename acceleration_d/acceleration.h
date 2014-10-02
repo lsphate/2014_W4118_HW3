fec6b9593d4d84f64cfe2f0d99e8468b8c965b37
Hao-Hsiang Chuang <hc2751@columbia.edu>
2014-10-02 16:53:45 -0400

    acceleration

---
+++ acceleration_d/acceleration.h
@@ -0,0 +1,22 @@
+#ifndef _ACCELERATION_H
+#define _ACCELERATION_H
+
+#include <linux/unistd.h>
+
+struct dev_acceleration{
+        int x;
+        int y;
+        int z;
+};
+
+struct acc_motion {
+
+     unsigned int dlt_x; /* +/- around X-axis */
+     unsigned int dlt_y; /* +/- around Y-axis */
+     unsigned int dlt_z; /* +/- around Z-axis */
+
+     unsigned int frq;   /* Number of samples that satisfies:
+                          sum_each_sample(dlt_x + dlt_y + dlt_z) > NOISE */
+};
+
+#endif
