94df4e34390f41b3f07931b45b551288025f26a1
Hao-Hsiang Chuang <hc2751@columbia.edu>
2014-10-06 18:13:50 -0400

    system call 378

--- flo-kernel/arch/arm/kernel/acceleration.c
+++ flo-kernel/arch/arm/kernel/acceleration.c
@@ -3,9 +3,15 @@
 #include <linux/syscalls.h>
 #include <linux/acceleration.h>
 
+struct dev_acceleration *data;
+
 asmlinkage int sys_set_acceleration(struct dev_acceleration __user * acceleration)
 {
-        printk("Congrats, your new system call has been called successfully");
+	//struct dev_acceleration *data;
+	if (copy_from_user(data, acceleration, sizeof(dev_acceleration)))
+                return -EINVAL;
+	
+	printk("Congrats, your new system call has been called successfully");
         return 0;
 }
 
