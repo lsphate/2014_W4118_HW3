0a7eb3e8ce362df6df3403db48fbf861531f77bc
Sun-Yi Lin <sl3833@columbia.edu>
2014-10-07 23:20:11 -0400

    just a little modify

--- flo-kernel/arch/arm/kernel/acceleration.c
+++ flo-kernel/arch/arm/kernel/acceleration.c
@@ -5,7 +5,7 @@
 
 struct dev_acceleration *data;
 
-asmlinkage int sys_set_acceleration(struct dev_acceleration __user * acceleration)
+SYSCALL_DEFINE1(set_acceleration, struct dev_acceleration __user *, acceleration)
 {
 	//struct dev_acceleration *data;
 	if (copy_from_user(data, acceleration, sizeof(dev_acceleration)))
@@ -15,29 +15,27 @@ asmlinkage int sys_set_acceleration(struct dev_acceleration __user * acceleratio
         return 0;
 }
 
-asmlinkage int sys_accevt_create(struct acc_motion __user *acceleration)
+SYSCALL_DEFINE1(accevt_create, struct acc_motion __user *, acceleration)
 {
         printk("Congrats, your new system call has been called successfully");
         return 0;
 }
 
-asmlinkage int sys_accevt_signal(struct dev_acceleration __user * acceleration)
+SYSCALL_DEFINE1(accevt_signal, struct dev_acceleration __user *, acceleration)
 {
         printk("Congrats, your new system call has been called successfully");
         return 0;
 }
 
 
-asmlinkage int sys_accevt_wait(int event_id)
+SYSCALL_DEFINE1(accevt_wait, int, event_id)
 {
         printk("Congrats, your new system call has been called successfully");
         return 0;
 }
 
-asmlinkage int sys_accevt_destroy(int event_id)
+SYSCALL_DEFINE1(accevt_destroy, int, event_id)
 {
         printk("Congrats, your new system call has been called successfully");
         return 0;
 }
-
-      
