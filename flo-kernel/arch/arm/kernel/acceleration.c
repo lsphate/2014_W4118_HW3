355af385fe9a027e5fd2040a6dfdf6b84d195fc8
Angel Say <aas2215@columbia.edu>
2014-10-09 11:28:33 -0400

    added TODO for primitive

--- flo-kernel/arch/arm/kernel/acceleration.c
+++ flo-kernel/arch/arm/kernel/acceleration.c
@@ -6,32 +6,60 @@
 
 struct dev_acceleration *data;
 
+/*
+ * Set current device acceleration in kernel.
+ * Acceleration is pointer to sensor data in
+ * user space. Acceleration value is overwritten
+ * every time this is called.
+ */
 SYSCALL_DEFINE1(set_acceleration, struct dev_acceleration __user *, acceleration)
 {
 	struct dev_acceleration data;
 	if (copy_from_user(&data, acceleration, sizeof(struct dev_acceleration)))
                 return -EINVAL;
-	
-	printk("Congrats, your new system call has been called successfully");
+	printk("x: %d, y: %d, z: %d\n", data.x, data.y, data.z);	
         return 0;
 }
+
+/* Create an event based on motion
+ * If frq exceeds WINDOW, cap frq at WINDOW.
+ * Returns event_id on success and error on failure.
+ */
 
 SYSCALL_DEFINE1(accevt_create, struct acc_motion __user *, acceleration)
 {
+	/* TODO:
+	 * kmalloc a new struct acc_motion
+	 * copy_from_user the passed acc_motion
+	 * add it to a larger data structure (hash table?)
+	 */
         printk("Congrats, your new system call has been called successfully");
         return 0;
 }
 
+/* take sensor data from user and store in kernel
+ * calculate the motion event
+ * notify all events with matching event
+ * unlblock all processes waiting
+ */
 SYSCALL_DEFINE1(accevt_signal, struct dev_acceleration __user *, acceleration)
 {
+	/* TODO:
+	 * Take in dev_accleration and copy_from_user
+	 * Subtract from previous dev_acceleration stored
+	 * in buffer to get difference in acc. If exceeds noise
+	 * add to buffer of size WINDOW. Check events using updated
+	 * buffer. Notify events with frq exceeded. Unblock those
+	 * that match.
+	 */
         printk("Congrats, your new system call has been called successfully");
         return 0;
 }
-
 
 SYSCALL_DEFINE1(accevt_wait, int, event_id)
 {
-        printk("Congrats, your new system call has been called successfully");
+       
+	printk("Congrats, your new system call has been called successfully");
         return 0;
 }
 
