bd24d527d5a3b6acb2a02a44f30d0815c6a6ec68
Hao-Hsiang Chuang <hc2751@columbia.edu>
2014-10-10 11:21:57 -0400

    acceleration.h

--- flo-kernel/include/linux/acceleration.h
+++ flo-kernel/include/linux/acceleration.h
@@ -13,12 +13,19 @@ struct dev_acceleration{
 
 struct acc_motion {
 
-     unsigned int dlt_x; /* +/- around X-axis */
-     unsigned int dlt_y; /* +/- around Y-axis */
-     unsigned int dlt_z; /* +/- around Z-axis */
+	unsigned int dlt_x; /* +/- around X-axis */
+	unsigned int dlt_y; /* +/- around Y-axis */
+	unsigned int dlt_z; /* +/- around Z-axis */
      
      unsigned int frq;   /* Number of samples that satisfies:
                           sum_each_sample(dlt_x + dlt_y + dlt_z) > NOISE */
+};
+
+struct acc_motion_list {
+
+	struct list_head programnode;
+	struct acc_motion motionlist;
+
 };
 
 #endif
