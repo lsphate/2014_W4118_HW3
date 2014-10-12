bb10c98553bd1c758982ddbb662806a2cf6f5dfe
Sun-Yi Lin <sl3833@columbia.edu>
2014-10-12 00:26:41 -0400

    acctest pass checkpatch

--- test/acctest.c
+++ test/acctest.c
@@ -3,11 +3,16 @@
 #include <unistd.h>
 #include "acctest.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	printf("entering acctest\n");
-	struct acc_motion motionTest = {.dlt_x = 200, .dlt_y = 200, .dlt_z = 200, .frq = 10};
+	struct acc_motion motionTest = {
+		.dlt_x = 200, .dlt_y = 200, .dlt_z = 200, .frq = 10
+	};
 	int result;
+
 	result = syscall(379, &motionTest);
-	printf("result %d", result);
+	printf("acc_motion ID: %d\n", result);
+	syscall(382, result);
 	return 0;
 }
