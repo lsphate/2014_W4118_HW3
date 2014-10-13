a233bd78b11e9fc796ea45809879884779e46a35
Angel Say <aas2215@columbia.edu>
2014-10-13 15:31:03 -0400

    updated test program

--- test/acctest.c
+++ test/acctest.c
@@ -2,18 +2,36 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include "acctest.h"
+#include <stdlib.h>
+#include <sys/wait.h>
+
+void forkEvent(int dlt_x, int dlt_y, int dlt_z, int frq);
 
 int main(int argc, char **argv)
 {
-	printf("entering acctest\n");
-	struct acc_motion motionTest = {
-		.dlt_x = 10, .dlt_y = 100, .dlt_z = 10, .frq = 5
-	};
-	int result;
-
-	result = syscall(379, &motionTest);
-	printf("acc_motion ID: %d\n", result);
-	syscall(380, result);
-	printf("motion detected!");
+	forkEvent(200, 10, 10, 2);
+	forkEvent(10, 200, 10, 2);
+	int status;
+	while(wait(&status) > 0);
 	return 0;
+}
+
+void forkEvent(int dlt_x, int dlt_y, int dlt_z, int frq) {
+	int pid;
+	pid = fork();
+	if (pid < 0) {
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0) {
+		struct acc_motion motionTest = {
+			.dlt_x = dlt_x, .dlt_y = dlt_y, .dlt_z = dlt_z, .frq = frq
+		};
+		int result;
+		result = syscall(379, &motionTest);
+		printf("acc_motion ID: %d\n", result);
+		syscall(380, result);
+		printf("%d detected a motion!\n", getpid());
+		syscall(382, result);
+		exit(EXIT_SUCCESS);
+	}
 }
