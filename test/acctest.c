12f557c821ed7e3c90725ce08347bde733528c9a
Angel Say <aas2215@columbia.edu>
2014-10-13 03:38:59 -0400

    updated test

--- test/acctest.c
+++ test/acctest.c
@@ -7,12 +7,13 @@ int main(int argc, char **argv)
 {
 	printf("entering acctest\n");
 	struct acc_motion motionTest = {
-		.dlt_x = 10, .dlt_y = 200, .dlt_z = 10, .frq = 10
+		.dlt_x = 10, .dlt_y = 100, .dlt_z = 10, .frq = 5
 	};
 	int result;
 
 	result = syscall(379, &motionTest);
 	printf("acc_motion ID: %d\n", result);
-	/*syscall(380, result);*/
+	syscall(380, result);
+	printf("motion detected!");
 	return 0;
 }
